3b36c6ee0264b3051f1ec0159584cd7a87b68b3e
AbhinayAmbati <abhinayambati4@gmail.com>
2025-11-07 12:19:23 +0530

    Update package.json URLs and enhance exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Bunpress } from "./core/app";
-
+export { Router } from "./core/router";
+export { BunResponse } from "./core/response";
+export { MiddlewareManager } from "./core/middleware";
